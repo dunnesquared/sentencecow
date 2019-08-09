6f6fd3ad0db10044145479fd671c32f346844db9
Math Woods <mathwoodstutor@gmail.com>
2019-08-09 11:43:49 -0400

    Refactor code that prevents user from sending no text

--- static/count.js
+++ static/count.js
@@ -1,31 +1,20 @@
-//alert("Hello World")
+// To do:
+// Make enableSubmit more responsive - doesn't disable button if just detelete all contents but don't click down later
 
-/*
+
+const WORD_MAX = 150;
+let textarea = document.querySelector("textarea");
+
+// Ensure that user can't submit an empty textarea to web script on first load
+textarea.addEventListener("load", enableSubmit());
+
+
+
 function sayHello(){
   alert("Hello World Pizza!!")
   return true;
 }
-*/
 
-
-/**
- * Enables submit button if text in textarea;
- * Disables submit button if textarea empty.
-*/
-function enableSubmit(){
-
-  let countButton = document.getElementById('count');
-  const text = document.getElementsByName("input_text")[0].value;
-
-  if (countWords(text)){
-    countButton.disabled = false;
-  }else{
-    countButton.disabled = true;
-  }
-}
-
-
-const WORD_MAX = 150;
 
 function countWords(text){
   let words = text.match(/\S+/g);
@@ -33,8 +22,9 @@ function countWords(text){
 }
 
 
-function check(){
 
+
+function checkWordCount(){
   //Get input text
   let text = "";
   let numWords = 0;
@@ -60,5 +50,84 @@ function check(){
     alert("Your text is over " + WORD_MAX + " words long.");
   }else{
     alert("Looks good!");
+  }
+}
+
+
+
+function check(){
+  //Get input text
+  let text = "";
+  let numWords = 0;
+
+  try{
+    text = document.getElementsByName("input_text")[0].value; //have to specify index as there is more than element by that name
+    //console.log(text); //NOT WORKING! Giving undefined! try using id instead
+
+    //Get number of words in text
+    numWords = countWords(text);
+    console.log("numWords =  " + numWords);
+  }
+  catch(err){
+    console.log(err.message);
+  }
+
+  //See whether number of words is greater than WORD_MAX
+  //True -> show alert; deactivate submit button
+  //False -> carry on
+  if (numWords > WORD_MAX){
+    let count_button = document.getElementById('count');
+    //count_button.disabled = true;
+    alert("Your text is over " + WORD_MAX + " words long.");
+  }else{
+    alert("Looks good!");
+  }
+}
+
+
+
+/**
+ *
+ *
+ *
+*/
+function updateWordCount(){
+    let text = "";
+    let numWords = 0;
+
+    try{
+      text = document.getElementsByName("input_text")[0].value; //have to specify index as there is more than element by that name
+      //console.log(text); //NOT WORKING! Giving undefined! try using id instead
+
+      //Get number of words in text
+      numWords = countWords(text);
+      console.log("numWords =  " + numWords);
+
+      document.getElementById('word-count').innerHTML = numWords;
+
+    }
+    catch(err){
+      console.log(err.message);
+    }
+}
+
+/**
+ * Enable submit button if text in textarea;
+ * Disable submit button if textarea empty.
+*/
+function enableSubmit(){
+
+  let countButton = document.getElementById('count');
+  const text = document.getElementsByName("input_text")[0].value;
+
+  //console.log("IN enableSubmit");
+
+
+  if (countWords(text)){
+    console.log("ENABLED!!");
+    //countButton.disabled = false;
+  }else{
+    console.log("DISABLED!!");
+    //countButton.disabled = true;
   }
 }
