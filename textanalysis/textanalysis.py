7d24ef62a72750176e54e270ff6c2f6e06116e42
Alex Dunne <alexdunne@gmail.com>
2019-10-31 19:43:57 -0400

    Improve docstrings and comments of public API

--- textanalysis/textanalysis.py
+++ textanalysis/textanalysis.py
@@ -2,8 +2,8 @@
 """Support to extract sentences from a text.
 
 This module provides functions to extract sentences from a text. Any string of
-alphanumeric characters that ends with a terminating punctuation mark such as a
-period, exclamation or question mark followed by a whitespace character is
+characters that ends with a terminating punctuation mark such as a period,
+exclamation or question mark followed by a whitespace character is
 considered to be a sentence, regardless of its grammatical correctness.
 Analyzed texts are assumed to be in English; all functions were written and
 tested with that assumption. Where decisions had to be made to respect American
@@ -85,12 +85,13 @@ import re   # sub
 import textwrap  # dedent
 import string # punctuation
 
+#==============================GLOBAL CONSTANTS=================================
 
 # Arbitrary text size set to avoid program from gobbling up too much memory
 # Set to what you will...
 MAX_TEXTSIZE = 100 * 1024 * 1024 # 100 MB
 
-# Check global just in case the bizarre happens...
+# Check global constant just in case the bizarre happens...
 if MAX_TEXTSIZE < 0:
     raise ValueError("MAX_TEXTSIZE set to value less than zero.")
 
@@ -144,12 +145,39 @@ REGEX_QUOTE = r'[\.\?!—' + LEADERS + QEX  + ']"\s'
 REGEX_DQUOTE = r'[\“\”' + DQUOTES  +  ']'
 
 # To be removed when counting words
-REGEX_ALLSYMOBLS = r'[' + string.punctuation + LEADERS + QEX + DQUOTES + SQUOTES +']'
+REGEX_ALLSYMOBLS = (r'[' + string.punctuation + LEADERS + QEX + DQUOTES
+                    + SQUOTES +']')
 
-#==============================ABBREVIATIONS====================================
+#===================INITIALIZING ABBREVIATIONS SET==============================
+
+def _get_dir():
+    """Returns absolute path of the directory where module exists
+
+    Args:
+        None
+
+    Returns:
+        dir (str): the unique ('canonical') absolute path of the directory
+                   containing the module (i.e. no symbolic links in path)
+    """
+
+    # Get the current working directory in Terminal
+    # when you try to launch the module as a script
+    cwd = os.getcwd()
+
+    # Get the name of the directoy where the module exists
+    module_dir = os.path.dirname(__file__)
+
+    # Intelligently cocantenate the two
+    joinedpath = os.path.join(cwd, module_dir)
+
+    # Get rid of any possible symbolic links found along and return the
+    # absolute path
+    return os.path.realpath(joinedpath)
+
 
 def _load_abbreviations():
-    '''Return list of abbreviations as per contents of abbreviations.txt
+    """Gets list of abbreviations as per contents of 'abbreviations.txt'
 
     Args:
         None
@@ -158,10 +186,9 @@ def _load_abbreviations():
         FileNotFoundError: abbreviations.txt not found
 
     Returns:
-        abbreviations (list): list of common English abbreviations
-    '''
+        abbreviations (list): strings found in 'abbreviations.txt'
+    """
 
-    # File to be read
     input_filename = "abbreviations.txt"
 
     # Get directory where input exists, i.e. same dir as this module
@@ -170,81 +197,56 @@ def _load_abbreviations():
     # Intelligently concatenate the directory and the input file name together
     full_filename = os.path.join(absdir, input_filename)
 
-    # Read input file
-    # 'with as' ensures file is closed even if exception raised
     with open(full_filename, "r") as fin:
         data = fin.read()
 
-    # Parse data and put into list
+    # Each abbreviation is written on a newline
     abbreviations = data.split('\n')
 
-    # Get rid of extra '' entry caused by text editor inexplicably adding
+    # Get rid of extra '' list-entry caused by text editor adding
     # a line after the last abbreviation upon saving the file
     abbreviations.pop()
 
     return abbreviations
 
 
-def _get_dir():
-    '''Return absolute path of the directory where script exists
-
-    Args:
-        None
-
-    Returns:
-        dir (str): the unique ('canonical') absolute path of the directory
-                   (i.e. no symbolic links in path)
-    '''
-
-    # Get the current working directory in Terminal
-    # when you try to launch the script
-    cwd = os.getcwd()
-
-    # Get the name of the directoy where this script exists
-    script_dir = os.path.dirname(__file__)
-
-    # Intelligently cocantenate the two
-    joinedpath = os.path.join(cwd, script_dir)
-
-    # Get rid of any possible symbolic links found along and return the
-    # absolute path
-    return os.path.realpath(joinedpath)
-
-
 # Common abbreviations found in English language
-# Casting them as a set will allow us perform an intersection with other data
+# Casting them as a set will allow efficient intersection with other data
 ABBREVIATIONS = set(_load_abbreviations())
-#==============================================================================
+
+#==============================CLASSES==========================================
 
 class NotInTextError(Exception):
-    '''Exception for instances when sentence is not in a text
+    """String not found in text.
 
     Attributes:
-        message (str): Message showing string that was not found in text.
-    '''
+        message (str): Message regarding string that was not found in a text.
+    """
 
     def __init__(self, message):
+        """Init parent class and this class with developer errpr message"""
         super().__init__(message)
         self.message = message
 
 
+#==============================PUBLIC FUNCTIONS=================================
+
 def get_sentences(text):
-    '''Return a list of sentences from a text written in English.
+    """Returns a list of sentences from a text.
 
-    Note that this algorithms does sentence extraction as best as possible.
-    Certain sentences may not be parsed correctly. For example,
+    Any string of characters that ends with a terminating punctuation mark such
+    as a period, exclamation or question mark followed by a whitespace character
+    is considered to be a sentence, regardless of its grammatical correctness.
 
-                "He ate a donut?" Alex asked.
-
-    does not parse to one sentence, but to two: "He ate a donut?"
-    and "Alex said."
+    Read the module docstring above for special cases that discuss the
+    limitations of this feature.
 
     Args:
         text (str): Text from which sentences are to be extracted.
 
     Returns:
         sent_list (list): A sequence of sentences extracted from argument.
-    '''
+    """
 
     print("***********DEBUGGING GET_SENTENCES***************")
     print("=================================================")
@@ -262,13 +264,11 @@ def get_sentences(text):
 
     # Set it up...
     # i (int): index of the end of a sentence
-    # start (int): index where parsing begins; moves as each sentence extracted
     # sentence (str): extracted sentence from text
     # sent_list (list): list of extracted sentences
-    #i, start, sentence, sent_list = 0, 0, "", []
     i, sentence, sent_list = 0, "", []
 
-    # Replace while start < len(text): it never became false
+    # Scan text...
     while start < (len(text)-1):
 
         i = _get_first_punctuation_mark(text, start)
@@ -277,9 +277,10 @@ def get_sentences(text):
         if i == -1:
             return sent_list
 
-        # find() returns the index of the first character in the string your
-        # searching for.
-        # As such increment the index if sentence ends with a quotation mark
+        # _get_first_punctuation_mark returns the index of a terminating
+        # punctuation mark, which in a sentence that ends in a quotation mark
+        # is does not point to the sentence's end.
+        # As such increment the index if sentence ends with a quotation mark.
         pos_lastchar = i
         if text[i+1] == "\"":
             pos_lastchar = i + 1
@@ -289,30 +290,20 @@ def get_sentences(text):
 
         print(f"sentence, before dedent/strip = {repr(sentence)} ")
 
-        # Clean up each sentence so we're not giving any extra spaces on either
-        # side
-
-        # BUG!!
-
-        # sentence = textwrap.dedent(sentence).strip()
-        #sentence = sentence.strip()
-
         print(f"sentence, after dedent/strip = {repr(sentence)} ")
 
-
-        # Add it to your list
+        # Add extracted sentence to list
         sent_list.append(sentence)
 
-        # Your next sentence starts one character away from the end of the
-        # previous sentence (in many langauges there is a space before the
-        # first letter of a sentence)
+        # The next sentence starts one character away from the end of the
+        # previous sentence
         start = pos_lastchar + 1
 
     return sent_list
 
 
 def get_words(sentence):
-    '''Return words in a sentence, excluding certain punctuation marks.
+    """Returns words in a sentence, excluding certain punctuation marks.
 
     The punctuation marks excluded are . , ! ? : ; “ ” " . Hyphens and
     apostrophes are kept, but em dashes are not.
@@ -322,13 +313,14 @@ def get_words(sentence):
 
     Returns:
         words (list): Sequence of words from the given sentence
-     '''
+     """
 
     # Remove all symbols and punctuation from sentence
-    # Do not something like "? ^ & * -" to count as five different words:
-    # should return no words
+    # e.g. Do not let something like "? ^ & * -" to count as five different
+    # words: function should return no words
     sentence = re.sub(REGEX_ALLSYMOBLS, '', sentence)
-    # Only symbol not removed from above. Don't know why...
+
+    # Only symbol not removed from above. Not sure why not...
     sentence = sentence.replace('\\', '')
 
     # Remove en dash – and em dash —
@@ -347,7 +339,7 @@ def get_words(sentence):
 
 
 def find_start_end(substring, text, start_search=0):
-    '''Return start and end indices of a substring within a given text.
+    """Returns start and end indices of a substring within a given text.
 
     Args:
         substring (str): Substring to search within another string
@@ -357,19 +349,18 @@ def find_start_end(substring, text, start_search=0):
     Raises:
         ValueError: Arguments substring and text are empty strings
 
-    Return:
+    Returns:
         -1 (int): If substring not in text being searched
-        (start_pos, end_pos) tuple(int, int): If substring is found,
-        the start and end indices of the substring in the searched string
-    '''
+        (start_pos, end_pos): An integer tuple representing the start and end
+                              indices of the substring in the searched string
+    """
 
     print("***********DEBUGGING FIND_START_END***************")
     print("==================================================")
     print("")
-
-    # NEW CODE
-    # Don't bother to find empty substrings in possibly empty texts
     print(f"\nDEBUG: PRE-STRIP, substring = {repr(substring)}")
+
+    # Don't bother to find empty substrings in possibly empty texts
     sublen = len(substring.strip())
     textlen = len(text.strip())
 
@@ -378,14 +369,13 @@ def find_start_end(substring, text, start_search=0):
                          "empty string(s) passed to parameters 'substring' " +
                          "or 'text'.")
 
-    # Substrings came from cleaned text. You won't get matches unless you
-    # make sure your text is cleaned too.
-
     #DEBUG
     print("\nBEFORE CLEANING/RE\n=====================")
     print(f"DEBUG: find_start_end, text = {repr(text)}")
     print(f"\nDEBUG: find_start_end, substring = {repr(substring)}")
 
+    # Substrings came from cleaned text. You won't get matches unless you
+    # make sure your text is cleaned too.
     text = _clean_text(text)
 
     # Clean the substring too of curly quotes
@@ -395,7 +385,6 @@ def find_start_end(substring, text, start_search=0):
     print("\nAFTER CLEANING/RE\n=====================")
     print(f"\nDEBUG: find_start_end, text = {repr(text)}")
     print(f"\nDEBUG: find_start_end substring, = {repr(substring)}\n")
-
 
     # Make sure our start position is something sensible
     if start_search < 0:
@@ -411,45 +400,36 @@ def find_start_end(substring, text, start_search=0):
     start_pos = 0
     end_pos = 0
 
-    # Find out start and end positions of substring in text
+    # Find or calculate the start and end positions of substring in text
     start_pos = text.find(substring, start_search, len(text) + 1)
     end_pos = start_pos + len(substring)
 
     return (start_pos, end_pos)
 
-    # DEPRECATED CODE
-    # Too complicated to escape all metacharacters in regex module
-    # Get start and end positions of substring in text; None if not there
-
-    # Required so pattern-matching doesn't ignore the question mark at
-    # # end of string
-    # substring = re.sub('[\?]', r'\?', substring)
-    #
-    # m = re.search(substring, text)
-    #
-    # if m:
-    #     print(f"DEBUG: regex object search result = {m.group()}")
-    #     start, end = m.span()
-    # else:
-    #     raise NotInTextError(f"Substring '{substring}' not found in text.'")
-    #
-    # return (start, end)
 
 def offset(text):
-    '''Return index of first non white-space character from left.
+    """Returns index of first non-whitespace character.
+
     Args:
-        text (str): string to be analyzed for white-spaces
+        text (str): string to be analyzed for whitespaces
 
     Returns:
-        index (int): index of first non-white-space character; -1 if none found
-    '''
-    index = 0
+        index (int): index of first non-whitespace character; -1 if none found
+    """
+
+    index = 0 # Assume first character is not a whitespace
+
+    # \s == whitespace character, ^\s == NOT whitespace character
     match = re.search(r'[^\s]', text, re.IGNORECASE)
+
+    # -1 only really possible if whole text is blank or made of whitespace
+    # characters
     index = match.start() if match else -1
 
     return index
 
-# +++++++++++++++++++++++++++++PRIVATE++++++++++++++++++++++++++++++++++++++
+
+# +++++++++++++++++++++++++++++PRIVATE FUNCTIONS++++++++++++++++++++++++++++++++++++++
 # Private module helper functions
 
 def _clean_text(text):
