6b90f071cb2e8c1d7572dde6a1090e72432c42b2
Alex Dunne <alexdunne@gmail.com>
2019-11-10 11:42:04 -0500

    Setup project to download dependencies via pip  install

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
-try:
-    from setuptools import setuptools
-except ImportError:
-    from disutils.core import setup
+import setuptools
 
-config = {
-    'description': 'My Project',
-    'author': 'My Name',
-    'url': 'URL to get it at',
-    'download_url': 'WHere to download it.',
-    'author_email': 'My email',
-    'version': '0.1',
-    'install_requires': ['nose'],
-    'packages': ['NAME'],
-    'scripts': [],
-    'name': 'projectname'
-}
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name="sentencecow",
+        version="0.1.0",
+        author="Alexander Dunne",
+        author_email="alexdunne@gmail.com",
+        description="Sentence extractor and word counter",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="MIT",
+        url="https://github.com/dunnesquared/sentencecow",
+        packages=setuptools.find_packages(),
+    	install_requires=["flask",],
+    	test_suite='nose.collector',
+    	tests_require=['nose'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+        include_package_data=True,
+    )
